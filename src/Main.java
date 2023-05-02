3ed3252691b08aa2907345555e8e563258f028f8
Cylon_Neto_Learning <cylonn@ya.ru>
2023-05-02 19:25:42 +0500

    Task 2 ver_1.0

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Main {
     public static String PATH = "D://IO_Games/savegames/";
 
     public static void main(String[] args) {
+        String[] pathArray = {PATH + "game1.dat", PATH + "game2.dat", PATH + "game3.dat"};
+        String zipPath = PATH + "save.zip";
         GameProgress game1 = new GameProgress(100, 2, 1, 34.2);
         GameProgress game2 = new GameProgress(99, 15, 3, 6678.1);
         GameProgress game3 = new GameProgress(56, 12, 6, 7890.22);
         saveGame(PATH + "game1.dat", game1);
         saveGame(PATH + "game2.dat", game2);
         saveGame(PATH + "game3.dat", game3);
+        zipFiles(zipPath, pathArray);
 
 
     }
@@ -19,10 +22,33 @@ public class Main {
     public static void saveGame(String path, GameProgress gameProgress) {
         try (FileOutputStream fos = new FileOutputStream(path);
              ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-            byte[] bytes =  gameProgress.toString().getBytes();
+            byte[] bytes = gameProgress.toString().getBytes();
             oos.writeObject(gameProgress);
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
+
+    public static void zipFiles(String pathZip, String[] list) {
+
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(pathZip, true))) {
+            for (String path : list) {
+                FileInputStream fis = new FileInputStream(path);
+                File file = new File(path);
+                ZipEntry entry = new ZipEntry(file.getName());
+                zout.putNextEntry(entry);
+                byte[] buffer = new byte[fis.available()];
+                fis.read(buffer);
+                zout.write(buffer);
+                zout.closeEntry();
+                fis.close();
+                file.delete();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
+
+
+
